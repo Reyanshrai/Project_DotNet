9da6019b927c7eb6177588682d6516738d02f04c
VSah7079 <vk3815155@gmail.com>
2025-03-16 22:52:06 +0530

    Update AssemblyInformationalVersion to reflect latest build

--- Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08591110c4d18476fb8a7256267cb6b652c76de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a54495754b89b3123a063d952072b3819d12ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
