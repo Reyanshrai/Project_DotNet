e008bd0d51192a49d8a8536670cc0cc4c7ec2a75
VSah7079 <vk3815155@gmail.com>
2025-03-21 15:09:20 +0530

    No code changes made

--- Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28931addd519bcd07b3708c266133dcd85de8f1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71bdbab443326c8ec51099e3fa59fae2805900fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
